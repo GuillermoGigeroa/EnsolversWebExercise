2e15593b02b2d64bed5f8d8ee67894d7b2418c76
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2021-03-11 19:29:54 -0300

    Adding folder system

--- JS/behaviour.js
+++ JS/behaviour.js
@@ -34,16 +34,40 @@ function changeName_Folder(name){
 
 var folders = new Array();
 
+function addNewFolder(folder){
+    folders.push(folder);
+}
+
 $(function(){
     var item1 = new item("Item1",false);
     var item2 = new item("Item2",true);
     var item3 = new item("Item3",true);
     var item4 = new item("Item4",false);
     var folder1 = new folder("Carpeta1");
+    var folder2 = new folder("Carpeta2");
+    var folder3 = new folder("Carpeta3");
     folder1.addItem(item1, item2, item3, item4);
-    folders.push(folder1);
-    mostrarCarpeta(folder1);
+    folder2.addItem(item2, item1, item4, item3);
+    folder3.addItem(item4, item3, item2, item1);
+    addNewFolder(folder1);
+    addNewFolder(folder2);
+    addNewFolder(folder3);
+    folders.forEach(folder => {
+        mostrarCarpeta(folder);
+    });
+    loadFolders();
 });
+
+function loadFolders(){
+    folders.forEach(folder => {
+        $("#folders").append(
+            "<li><ul class=\"folders1\"><li>"
+            +folder.name
+            +"</li><li><button>View Items</button></li>"
+            +"<li><button>Remove</button></li>"
+            +"</ul></li>");
+    });
+}
 
 //Develop testing
 function mostrarCarpeta(carpeta){
@@ -56,3 +80,7 @@ function mostrarCarpeta(carpeta){
     });
     console.log("---------------------------------------------------");
 }
+
+$("#toggleViewFolders").click(function() { 
+    $("#foldersBox").toggle();
+});
